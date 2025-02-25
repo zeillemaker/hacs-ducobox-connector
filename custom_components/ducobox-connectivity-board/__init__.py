a25facc73f4f18a772f9cd6161ab632fb3663dff
T Phil <thomas@tphil.nl>
2025-02-25 07:09:50 +0100

    Revert "Merge pull request #17 from arvdrpoo/fix-deprecations"
    This reverts commit 64909e1ecc93178fbc6d3b87d72dfd783be2fd76, reversing
    changes made to d1e6f70703ae5b2274f5b99fd53750e81471a663.

--- custom_components/ducobox-connectivity-board/__init__.py
+++ custom_components/ducobox-connectivity-board/__init__.py
@@ -25,7 +25,7 @@ async def async_setup_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
         _LOGGER.error("Could not connect to Ducobox: %s", ex)
         raise ConfigEntryNotReady from ex
 
-        await hass.config_entries.async_forward_entry_setups(entry, ["sensor"])
+    await hass.config_entries.async_forward_entry_setup(entry, "sensor")
     return True
 
 async def async_unload_entry(hass: HomeAssistant, entry: ConfigEntry) -> bool:
